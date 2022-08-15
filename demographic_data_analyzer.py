88565e61f5997befdc87294992a0b9a43643ee42
Chinmay Nagesh Jathar <96647640+Batman2002@users.noreply.github.com>
2022-08-15 09:21:58 +0000

    Male average age done and race count almost done

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,38 +6,54 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    df["index"]=1
+    df=df.filter(items=["race","index"])
+    df=df.groupby("race").sum()
+ # df
+    df.to_dict()
+    
     race_count = None
-
+    df = pd.read_csv("adult.data.csv")
     # What is the average age of men?
-    average_age_men = None
+    df=df.filter(items=["sex","age"]) 
+    df=df.groupby("sex").mean()
+    df.to_dict()
+    average_age_men = df["age"]["Male"]
 
     # What is the percentage of people who have a Bachelor's degree?
+    df = pd.read_csv("adult.data.csv")
     percentage_bachelors = None
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
+    df = pd.read_csv("adult.data.csv")
     higher_education = None
     lower_education = None
 
     # percentage with salary >50K
+    df = pd.read_csv("adult.data.csv")
     higher_education_rich = None
     lower_education_rich = None
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    df = pd.read_csv("adult.data.csv")
     min_work_hours = None
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    df = pd.read_csv("adult.data.csv")
     num_min_workers = None
 
     rich_percentage = None
 
     # What country has the highest percentage of people that earn >50K?
+    df = pd.read_csv("adult.data.csv")
     highest_earning_country = None
     highest_earning_country_percentage = None
 
     # Identify the most popular occupation for those who earn >50K in India.
+    df = pd.read_csv("adult.data.csv")
     top_IN_occupation = None
 
     # DO NOT MODIFY BELOW THIS LINE
